09fd820f5fe3d845ba90f212d3f0c71b7362d37a
Eastep <easandb@dunwoody.edu>
2020-12-18 15:31:24 -0600

    comments

--- app.py
+++ app.py
@@ -3,36 +3,37 @@ from flask import Flask
 from flask import render_template
 from flask_pymongo import PyMongo
 
-app = Flask(__name__)
+app = Flask(__name__) # Initalize flask 
 
 #mongo
-app.config["MONGO_URI"] = "mongodb://192.168.56.223:27017/temperature"
-mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb://192.168.56.223:27017/temperature" # Configure mongo url
+mongo = PyMongo(app) # initialize pymongo with mongo url
 
-@app.route('/') 
+# functions will run based on which route is called (function below route declaration runs)
+@app.route('/') # allow base route
 @app.route('/index/')
 def index():
     return render_template('index.html')
-@app.route("/get_one_temp_api")
+@app.route("/get_one_temp_api") # get one temperature reading
 def temp1():
     one_temp = mongo.db.temperature.find_one()
     #print(temp_reading)
     return str(one_temp)
 
 @app.route("/get_ten_temps_api")
-def temp10():
+def temp10(): # query db to get temperature readings
     temps = ""
     ten_temps = mongo.db.temperature.find().limit(10)
     for temp in ten_temps:
         temps=temps+str(temp)
         print(temps)
-    return temps
+    return temps 
 
 @app.route("/recent_temps")
 def recent():
     temps = mongo.db.temperature.find().limit(10)
     
-    return render_template('temps.html',temps=temps)
+    return render_template('temps.html',temps=temps) # Render template with temps list as variable
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyonepy
--- test.py
+++ test.py
@@ -4,19 +4,19 @@ import app
 
 class TestHello(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self): # Intialize app for testing
         app.app.testing = True
         self.app = app.app.test_client() 
 
-    def test_index(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status, '200 OK')
+    def test_index(self): # Ensure base route works correctly
+        rv = self.app.get('/') # Get request on base route
+        self.assertEqual(rv.status, '200 OK') # Assert route status is equal to 200
 
-    def test_bad_route(self):
+    def test_bad_route(self): # Ensure non existent route returns 404
         rv = self.app.get('/badRoute')
         self.assertEqual(rv.status, '404 NOT FOUND')
 
-    def test_get_one_temp(self):
+    def test_get_one_temp(self): # Ensure get one temp route works, also ensures db connection is live
         rv = self.app.get('/get_one_temp_api')
         self.assertEqual(rv.status, '200 OK')
 
